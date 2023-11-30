356d8d773358ddec3099b53080a5dbaa951e49ef
SolangeD11 <SolangeD11>
2023-11-29 23:40:26 -0500

    Solange

---
+++ Practica-Vite/Vite-project/src/interfaces/Iidioma.ts
@@ -0,0 +1,6 @@
+export interface Iidioma {
+    id?: number;
+    nombreidioma: string;
+    origenId: string;
+}
+
---
+++ Practica-Vite/Vite-project/src/main.ts
@@ -0,0 +1,110 @@
+import './style.css'
+
+
+
+
+(async ()=>{
+
+  const response = await fetch('http://localhost:3000/api/idioma')
+  const data = await response.json()
+  
+  let divTable= `<table>`
+  divTable += `<tr><th>Id</th><th>code</th><th>Detail</th><th>Acciones</th></tr>`
+  data.forEach((idioma: Iidioma) => {
+    divTable += `<tr><td>${idioma.id}</td><td>${idioma.nombreidioma}</td><td><button class="btn btn-delete">Eliminar</button></td><td><button class="btn btn-update">Actualizar</button> </td> </tr>`
+  })
+  divTable += `</table>`
+
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = divTable
+
+
+  const divButton = `<button class="btn btn-primary" >Agregar</button>`
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML += divButton
+
+  const btnAgregar = document.querySelector<HTMLButtonElement>('.btn-primary')
+  btnAgregar?.addEventListener('click', ()=>{
+    const divForm = `<form>
+    <div class="mb-3">
+      <label for="code" class="form-label">Code</label>
+      <input type="text" class="form-control" id="code" aria-describedby="code">
+    </div>
+    <div class="mb-3">
+      <label for="detail" class="form-label">Detail</label>
+      <input type="text" class="form-control" id="detail">
+    </div>
+    <button type='submit'  class="btn btn-save">Save</button>
+    <button type='submit'  class="btn btn-cancel">Cancel</button>
+    </form>`
+    document.querySelector<HTMLDivElement>('#app')!.innerHTML = divForm
+    const btnSave = document.querySelector<HTMLButtonElement>('.btn-save')
+    btnSave?.addEventListener('click', async (e)=>{
+      e.preventDefault()
+      const nombreidioma = document.querySelector<HTMLInputElement>('#nombreidioma')!.value
+      const response = await fetch('http://localhost:3000/api/idioma', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({nombreidioma, origenId:1})
+      })
+      const data = await response.json()
+      console.log(data)
+      // reload page
+      location.reload()
+    })
+  })
+  document.querySelectorAll<HTMLButtonElement>('.btn-delete').forEach(btn=>{
+    btn.addEventListener('click', async ()=>{ 
+      const id = btn.parentElement?.parentElement?.firstElementChild?.textContent
+      console.log(id)
+      await fetch(`http://localhost:3000/api/idioma/${id}`, {
+        method: 'DELETE'
+      })
+      location.reload()
+
+     })
+  })
+  document.querySelectorAll<HTMLButtonElement>('.btn-update').forEach(btn=>{
+    btn.addEventListener('click', async ()=>{ 
+      const id = btn.parentElement?.parentElement?.firstElementChild?.textContent
+      const response = await fetch(`http://localhost:3000/api/idioma/${id}`)
+      const data = await response.json()
+      //add button for cancel
+      const btnCancel = `<button class="btn btn-cancel">Cancel</button>`
+      const divForm = `<form>
+      <div class="mb-3">
+        <label for="code" class="form-label">Code</label>
+        <input type="text" class="form-control" id="code" aria-describedby="code" value="${data.nombreidioma}">
+      </div>
+      <div class="mb-3">
+        <label for="detail" class="form-label">Detail</label>
+        <input type="text" class="form-control" id="detail" value="${data.origenId}">
+      </div>
+      <button type='submit'  class="btn btn-save">Save</button>
+      ${btnCancel}
+      </form>`
+      document.querySelector<HTMLDivElement>('#app')!.innerHTML = divForm
+      const btnSave = document.querySelector<HTMLButtonElement>('.btn-save')
+      btnSave?.addEventListener('click', async (e)=>{
+        e.preventDefault()
+        const nombreidioma = document.querySelector<HTMLInputElement>('#nombreidioma')!.value
+        const response = await fetch(`http://localhost:3000/api/idioma/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({nombreidioma, origenId:1})
+        })
+        const data = await response.json()
+        console.log(data)
+        // reload page
+        location.reload()
+      })
+     })
+  })
+
+  
+  
+
+
+})()
